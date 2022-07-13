927aec3abe62604d5e8d8f7bb954798e8374f512
Jacob Ratima <47646398+Rahtz@users.noreply.github.com>
2022-07-13 13:14:22 +1200

    Update Program.cs

--- BlockCity/Program.cs
+++ BlockCity/Program.cs
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
         builder =>
         {
-            builder.WithOrigins("https://blockcity.herokuapp.com")
+            builder.WithOrigins("http://localhost:4200",
+                                        "https://blockcity.herokuapp.com")
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
